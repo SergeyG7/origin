118dbdeecc3f8e91f43903db2163acf201990dae
Сергей <gvozd0770@gmail.com>
2024-12-17 19:44:22 +0200

    Initial commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,26 @@
 package app;
 
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        int a;
+        System.out.print("Введіть сумму свого доходу: ");
+        double income = scanner.nextDouble();
+        double taxRate = 0;
+
+        if (income <= 10000){
+            taxRate = 2.5;
+        } else if (income > 10000 && income <= 25000) {
+            taxRate = 4.3;
+        } if (income > 25000){
+            taxRate = 6.7;
+        }
+        double taxAmount = income * (taxRate / 100);
+
+        System.out.printf("Сумма податку складає: %.2f\n", taxAmount);
+
+
     }
 }
